accd7f36cdc31ab81fc8151fbfc1a85b53f7bee6
sebous <sebous@gmail.com>
2022-01-31 17:50:10 +0100

    example

--- declaration/index.d.ts
+++ declaration/index.d.ts
@@ -1,5 +1,3 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 export { Bs } from "./components/Bs";
 export { NoLibComponent } from "./components/NoLibComponent";
-export * as utils from "./utils";
 export { Select } from "./components/Select";
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-
 export { Bs } from "./components/Bs";
 export { NoLibComponent } from "./components/NoLibComponent";
-export * as utils from "./utils";
+// export * from "./utils";
 export { Select } from "./components/Select";
